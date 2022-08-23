559ddf0682d6909f4f343dfa601fbde8cb33e1af
Michał S <9sosna9@wp.pl>
2022-08-23 21:22:41 +0200

    djhd

---
+++ coinflip.py
@@ -0,0 +1,15 @@
+import random
+random.randint(0, 1)
+def flip():
+    coin_side=random.randint(0, 1)
+    if coin_side == 0 :
+        print('Heads')
+    else:
+        print('Tails')
+while(1):
+    print('wanna toss a coin?')
+    if(bool(input()) == 1):
+        flip()
+    else:
+        print('shame?')
+        break
--- dupa.py
+++ dupa.py
@@ -1,1 +1,11 @@
 #dupa
+def print_checker(n):
+    is_prime=True
+    for dupa in range(2, n):
+        if(n%dupa==0)
+            is_prime=False
+            print('NIEPIERWSZAKURWA')
+     print('pierwsza kurwa') if(is_prime)
+
+n=int(input('sprawdzanie pierwszych'))
+prime_checker(n)
