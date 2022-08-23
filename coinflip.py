0073aee7923f958f8705dc218595bc843520e5bc
Michał S <9sosna9@wp.pl>
2022-08-23 21:51:18 +0200

    djhd

--- coinflip.py
+++ coinflip.py
@@ -1,16 +1,19 @@
 import random
 random.randint(0, 1)
+
+
 def flip():
-    coin_side=random.randint(0, 1)
-    if coin_side == 0 :
+    coin_side = random.randint(0, 1)
+    if coin_side == 0:
         print('Heads')
     else:
         print('Tails')
-while(1):
-    print('wanna toss a coin?')
-    if(bool(input()) == 1):
-        flip()
-    else:
-        print('shame?')
-        break
 
+
+while 1:
+    print('wanna toss a coin?')
+    if bool(input()) == 1:
+        flip()
+    else:1
+        print('shame')
+        break
--- dupa.py
+++ dupa.py
@@ -1,11 +1,12 @@
 #dupa
-def print_checker(n):
+def prime_checker(n):
     is_prime=True
     for dupa in range(2, n):
-        if(n%dupa==0)
+        if(n%dupa==0):
             is_prime=False
             print('NIEPIERWSZAKURWA')
-     print('pierwsza kurwa') if(is_prime)
-
-n=int(input('sprawdzanie pierwszych'))
-prime_checker(n)
+            break
+    if (is_prime): print('pierwsza kurwa')
+while 1:
+    n=int(input('sprawdzanie pierwszych'))
+    prime_checker(n)
