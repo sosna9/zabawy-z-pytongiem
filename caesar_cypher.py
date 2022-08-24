481492ca1931542437df66904313d4e62e6600cd
Michał S <9sosna9@wp.pl>
2022-08-24 16:58:09 +0200

    caesar cypher

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -4,32 +4,32 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 class Encryptor:
     def __init__(self, tekst):
-        self.__tekst = tekst
+        self.tekst_2 = tekst
 
     def encryption(self):
         shift = int(input('podaj przesunięcie'))
         encrypted = ''
-        for letter in self.__tekst:
+        for letter in self.tekst_2:
             if not(letter in alphabet):
                 encrypted += letter
                 continue
             position = alphabet.index(letter)
             new_pos = position + shift
-            encrypted += alphabet[new_pos]
-        self.__tekst = encrypted
+            encrypted += alphabet[new_pos%26]
+        self.tekst_2 = encrypted
         print(encrypted)
 
     def decryption(self):
         shift = int(input('podaj przesunięcie'))
         decrypted = ''
-        for letter in self.__tekst:
+        for letter in self.tekst_2:
             if not (letter in alphabet):
                 decrypted += letter
                 continue
             position = alphabet.index(letter)
             new_pos = position - shift
             decrypted += alphabet[new_pos]
-        self.__tekst = decrypted
+        self.tekst_2 = decrypted
         print(decrypted)
 
 
