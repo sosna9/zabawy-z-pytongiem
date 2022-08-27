91ddc7c54273af823c5da2c371a81a2c8b32d077
Michał S <9sosna9@wp.pl>
2022-08-27 18:41:48 +0200

    caesar cypher

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -4,37 +4,39 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 class Encryptor:
     def __init__(self, tekst):
-        self.tekst_2 = tekst
+        self.__tekst = tekst
 
     def encryption(self):
         shift = int(input('podaj przesunięcie'))
         encrypted = ''
-        for letter in self.tekst_2:
+        for letter in self.__tekst:
             if not(letter in alphabet):
                 encrypted += letter
                 continue
             position = alphabet.index(letter)
             new_pos = position + shift
             encrypted += alphabet[new_pos%26]
-        self.tekst_2 = encrypted
+        self.__tekst = encrypted
         print(encrypted)
 
     def decryption(self):
         shift = int(input('podaj przesunięcie'))
         decrypted = ''
-        for letter in self.tekst_2:
+        for letter in self.__tekst:
             if not (letter in alphabet):
                 decrypted += letter
                 continue
             position = alphabet.index(letter)
             new_pos = position - shift
-            decrypted += alphabet[new_pos]
-        self.tekst_2 = decrypted
+            decrypted += alphabet[new_pos%26]
+        self.__tekst = decrypted
         print(decrypted)
 
 
 enigma = Encryptor(input('podaj wiadomość'))
 enigma.encryption()
 enigma.decryption()
+enigma.__tekst= 'chuuuj'
+print(enigma.__tekst)
 
 
