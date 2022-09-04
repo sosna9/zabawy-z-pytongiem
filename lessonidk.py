8dd7c34d5eb13db43c968eba3e1d60416bb5b5ee
Michał S <9sosna9@wp.pl>
2022-09-04 23:28:38 +0200

    caesar cypher

--- lessonidk.py
+++ lessonidk.py
@@ -1,15 +1,26 @@
 from turtle import *
-
+from random import *
 timmy = Turtle()
 timmy.shape("turtle")
 
 screen = Screen()
 
 
+def draw(lil, sides):
+    for _ in range(sides):
+        lil.forward(100)
+        lil.right(360 / sides)
+
+
+timmy.clear()
+for shape in range(3,10):
+    draw(timmy, shape)
+
+"""
+
 for i in range(4):
     timmy.forward(300)
     timmy.right(90)
-timmy.clear()
 for i in range(20):
     print(i, "\n")
     if (i) % 2 == 0:
@@ -17,4 +28,5 @@ for i in range(20):
     else:
         timmy.up()
     timmy.forward(20)
+    """
 screen.exitonclick()
