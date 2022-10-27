307ba50081dc71d9473e885125f7f4656924b963
sosna9 <9sosna9@wp.pl>
2022-10-27 14:57:33 +0200

    idk check

--- pong/ball.py
+++ pong/ball.py
@@ -1,14 +1,20 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
         self.color("yellow")
         self.shape("circle")
+        self.direction_x = 1
+        self.direction_y = 1
 
     def move(self):
-        print("exdi")
-        new_x = self.xcor() +10
-        new_y = self.ycor() +10
+        if self.ycor() > 280:
+            self.direction_y = -1
+        if self.ycor() < -280:
+            self.direction_y = 1
+        new_x = self.xcor() + 10 * self.direction_x
+        new_y = self.ycor() + 10 * self.direction_y
         self.goto(new_x, new_y)
--- pong/pong.py
+++ pong/pong.py
@@ -1,18 +1,18 @@
 from turtle import Screen
-from turtle import Turtle
 from paddle import Paddle
 from ball import *
 import time
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("pong")
 screen.tracer(0)
+FONT = ('Courier', 24, 'normal')
 
 r_paddle = Paddle(360, 0)
 l_paddle = Paddle(-360, 0)
 ball = Ball()
-
 
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
@@ -21,13 +21,27 @@ screen.onkeypress(r_paddle.go_down, "Down")
 screen.onkeypress(l_paddle.go_up, "w")
 screen.onkeypress(l_paddle.go_down, "s")
 
-
 is_game_on = True
 
 while is_game_on:
+    time.sleep(0.03)
     screen.update()
     ball.move()
 
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 340) or (ball.distance(l_paddle) < 50 and ball.xcor() < -340):
+        ball.direction_x *= -1
+
+    if ball.xcor() > 380:
+        print("punkt")
+        ball.direction_x *= -1
+        ball.goto(0, 0)
+        time.sleep(0.5)
+    if ball.xcor() < -380:
+        print("punkt2")
+        time.sleep(0.5)
+        ball.direction_x *= -1
+        ball.goto(0, 0)
+        time.sleep(0.5)
 
 screen.exitonclick()
 print("testt")
