621135c495ac0020c7147beb0e9f36253c3607c2
sosna9 <9sosna9@wp.pl>
2022-11-03 15:50:44 +0100

    us game continue

--- usgame/._main.py
+++ usgame/._main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("zgaduj stany")
@@ -7,5 +8,27 @@ screen.addshape(image)
 
 turtle.shape(image)
 
+dupa = pandas.read_csv("50_states.csv")
+all_states = dupa.state.to_list()
+guessed_states = []
+
+
+while len(guessed_states)<50:
+    answer_state = screen.textinput(title="Dupa", prompt="daj no").title()
+
+
+    if (answer_state in all_states):
+        guessed_states.append(answer_state)
+        print("HURRA")
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = dupa[dupa.state == answer_state]
+        print(state_data)
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+    else:
+        print("Chuj")
 
 screen.exitonclick()
