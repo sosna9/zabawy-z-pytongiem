812f2587b64c0af67bd25d78c731c732fbab99b8
sosna9 <9sosna9@wp.pl>
2022-11-03 12:36:56 +0100

    squirrels

--- texty.py
+++ texty.py
@@ -1,3 +1,20 @@
-with open(r"C:\Users\msosnowski\myfile.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("weather_data.csv") as file:
+#     contents = file.readlines()
+#     print(contents)
+import pandas
+# import csv
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         print(row[1])
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+import numpy as np
+
+dupa = pandas.read_csv("weather_data.csv")
+
+print(dupa[dupa.temp > 15])
