e3e0ff19e51ab0c2d2f62f630e13cc3252e7d78f
sosna9 <9sosna9@wp.pl>
2022-11-04 15:47:41 +0100

    us game piontek

--- usgame/._main.py
+++ usgame/._main.py
@@ -10,13 +10,26 @@ turtle.shape(image)
 
 dupa = pandas.read_csv("50_states.csv")
 all_states = dupa.state.to_list()
+dfstate = pandas.read_csv("stany zgadniete")
 guessed_states = []
+x = dfstate.Stan.values.tolist()
+guessed_states += x
+print(guessed_states)
+
+for state in guessed_states:
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = dupa[dupa.state == state]
+    print(state_data)
+    t.goto(int(state_data.x), int(state_data.y))
+    t.write(state_data.state.item())
 
 
 while len(guessed_states)<50:
     answer_state = screen.textinput(title="Dupa", prompt="daj no").title()
 
-
+    if answer_state == "Exit" :break
     if (answer_state in all_states):
         guessed_states.append(answer_state)
         print("HURRA")
@@ -30,5 +43,7 @@ while len(guessed_states)<50:
 
     else:
         print("Chuj")
-
+print(guessed_states)
+df = pandas.DataFrame(guessed_states, columns=["Stan"])
+df.to_csv("stany zgadniete")
 screen.exitonclick()
